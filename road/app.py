4b6bc6b9dd28c2fb0fc5b115c8229632a04850bc
quppeq <quppeq@gmail.com>
2021-05-10 23:01:53 +0300

    skeleton project

---
+++ road/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+
+from flask_migrate import MigrateCommand
+from flask_script import Manager, Server
+
+from db import db
+
+
+def configure_db(app: Flask):
+    db.init_app(app)
+
+
+def configure_manager(app: Flask) -> Manager:
+    manager = Manager(app)
+    manager.add_command("runserver", Server(host='0.0.0.0', threaded=True))
+    manager.add_command('db', MigrateCommand)
+    return manager
+
+
+def create_app():
+    app = Flask(__name__)
+    configure_db(app)
+    app.manager = configure_manager(app)
+    return app
